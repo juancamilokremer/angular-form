d358f745d23fc6ac9195f9c20e64b5576322cf1a
Juan Camilo Kremer <juancamilokremer@gmail.com>
2023-05-07 22:59:05 -0500

    solución formcontrol, validaciones, estilos para errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FormcontrolExerciseComponent } from './components/formcontrol-exercise/formcontrol-exercise.component';
 import { FormgroupExerciseComponent } from './components/formgroup-exercise/formgroup-exercise.component';
 import { AppRoutingModule } from './app-routing.module ';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module ';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/formcontrol-exercise/formcontrol-exercise.component.ts
+++ src/app/components/formcontrol-exercise/formcontrol-exercise.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formcontrol-exercise',
   templateUrl: './formcontrol-exercise.component.html',
   styleUrls: ['./formcontrol-exercise.component.css']
 })
-export class FormcontrolExerciseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FormcontrolExerciseComponent {
+  name: FormControl = new FormControl('', Validators.required);
 }
